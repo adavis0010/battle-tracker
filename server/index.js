08f76d667739285f6f675e6e6b71213124d90bf4
Ash Davis <sadavis0010@gmail.com>
2021-10-22 17:13:57 -0700

    added server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express  = require("express");
+const app = express();
+
+app.use(express.json())
+
+app.use( express.static( `${__dirname}/../build` ) );
+
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
+
+let port = 3000
+app.listen(port, ()=> console.log(`Listening on port ${port}`))
